c4250f4107301dfc72c7e17849cd3643d7685ce0
marmar200 <marmar.2004@yandex.ru>
2025-04-16 16:13:05 +0300

    add create Post

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './styles/App.css';
 import React from 'react';
 import { useState } from 'react';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 
 function App() {
@@ -11,8 +13,39 @@ function App() {
     {id: 3, title: 'Go', body: 'Description'},
   ])
 
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const addNewPost = (e) => {
+      e.preventDefault();
+      const newPost = {
+        id: Date.now(),
+        title,
+        body
+      };
+
+      setPosts([...posts, newPost]);
+      setTitle('');
+      setBody('');
+  };
+
   return (
     <div className="App">
+        <form action="">
+            <MyInput 
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              type="text" 
+              placeholder='Название поста'
+            />
+            <MyInput 
+              value={body}
+              onChange={e => setBody(e.target.value)}
+              type="text" 
+              placeholder='Описание поста'
+            />
+            <MyButton onClick={addNewPost}>Создать пост</MyButton>
+        </form>
         <PostList posts={posts}/>
     </div>
   );
